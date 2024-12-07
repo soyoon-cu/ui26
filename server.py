335bcb6d7346568596c5846ee46ec4eb17841b5a
salmary <112910438+salmary@users.noreply.github.com>
2024-12-06 20:04:36 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,15 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+name = "Lisa"
+
 @app.route('/')
-def hello_world():
-   return 'Hello, World!'
+def landing():
+   return render_template('landing.html')
+
+@app.route('/home/<name>')
+def home(name=None):
+    return render_template('home.html', name=name) 
 
 if __name__ == '__main__':
    app.run(debug = True)

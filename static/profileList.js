da5e34d3f11442ef599c92953cb6b6e8bbb2d967
soyoon <sp4412@columbia.edu>
2024-12-15 16:27:14 -0500

    profile list done

--- static/profileList.js
+++ static/profileList.js
@@ -12,41 +12,34 @@ function handleDropdownChange() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     prefillDropdown();
     handleDropdownChange();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+    
     const profilesPerPage = 6; 
     let currentPage = 1;
     let filteredProfiles = [];
-    let allProfiles = [];
-  
+    let allProfiles = [];  
 
+    let category = window.location.pathname.split("/").pop();
+    
     fetch("/static/questionnaire_data.json")
       .then((response) => response.json())
       .then((data) => {
         allProfiles = data;
-        filteredProfiles = allProfiles;
+        if (category === "all") {
+            filteredProfiles = allProfiles
+        } else {
+            filteredProfiles = allProfiles.filter(
+                (profile) => profile.category.toLowerCase() === category
+            );
+        }
         renderProfiles();
       })
       .catch((error) => console.error("Error loading profiles:", error));
   
     const filterDropdown = document.querySelector(".filter-dropdown");
     filterDropdown.addEventListener("change", function () {
-      const selectedCategory = filterDropdown.value;
-      currentPage = 1;
-  
-      if (selectedCategory) {
-        filteredProfiles = allProfiles.filter(
-          (profile) => profile.category.toLowerCase() === selectedCategory
-        );
-      } else {
-        filteredProfiles = allProfiles;
-      }
-  
-      renderProfiles();
       updateUrl(selectedCategory);
     });
   
@@ -68,27 +61,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     function renderProfiles() {
-      const container = document.querySelector(".container .box-wrapper");
-      container.innerHTML = "";
-  
       const start = (currentPage - 1) * profilesPerPage;
       const end = Math.min(start + profilesPerPage, filteredProfiles.length);
       const profilesToDisplay = filteredProfiles.slice(start, end);
+
+      const boxes = document.querySelectorAll('.box2');
   
-      profilesToDisplay.forEach((profile) => {
-        const profileHtml = `
-          <a href="#" class="box2">
+      profilesToDisplay.forEach((profile, index) => {
+        const box = boxes[index];
+        box.innerHTML = `
             <p class="name"><b>${profile.working_area}</b></p>
             <div class="profile-card">
               <div class="icon-text"><i class="fas fa-user icon-large"></i> <span>${profile.name}</span></div>
               <div class="icon-text"><i class="fas fa-briefcase icon-large"></i> <span>${profile.current_job}</span></div>
               <div class="icon-text"><i class="fas fa-book icon-large"></i> <span>${profile.major}</span></div>
             </div>
-          </a>
         `;
-        container.insertAdjacentHTML("beforeend", profileHtml);
+        box.classList.remove("hidden");
+        box.addEventListener("click", () => {
+            window.location.href = `/profile/${profile.id}`;
+        });
       });
-  
+
+      for (let i = profilesToDisplay.length; i < boxes.length; i++) {
+        const box = boxes[i];
+        boxes[i].innerHTML = "";
+        box.classList.add('hidden');
+    }
       updateNavigationButtons();
     }
   

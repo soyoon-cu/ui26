9b8be7f684cf3239c34ade91028f5fc179c0b6f5
salmary <112910438+salmary@users.noreply.github.com>
2024-12-15 11:57:47 -0500

    some fix

--- static/careerCategories.js
+++ static/careerCategories.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const careerRedirects = {
-      "Graduate Education": "/profile_list/graduate-education",
-      "Consulting": "/profile_list/consulting",
-      "Finance": "/profile_list/finance",
-      "Tech": "/profile_list/tech",
-      "Public Sector": "/profile_list/public-sector",
-    };
-  
-    const optionButtons = document.querySelectorAll(".btn.option");
-    optionButtons.forEach((button) => {
-      button.addEventListener("click", function () {
-        const buttonText = this.textContent.trim();
-        if (careerRedirects[buttonText]) {
-          window.location.href = careerRedirects[buttonText];
-        }
-      });
-    });
-  
-    const backButton = document.querySelector(".btn.back-btn");
-    backButton.addEventListener("click", function () {
-      window.location.href = "/home"; 
+  const careerRedirects = {
+    "Graduate Education": "/profile_list/graduate-education",
+    Consulting: "/profile_list/consulting",
+    Finance: "/profile_list/finance",
+    Tech: "/profile_list/tech",
+    "Public Sector": "/profile_list/public-sector",
+  };
+
+  const optionButtons = document.querySelectorAll(".btn.option");
+  optionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const buttonText = this.textContent.trim();
+      if (careerRedirects[buttonText]) {
+        window.location.href = careerRedirects[buttonText];
+      }
     });
   });
+
+  const backButton = document.querySelector(".btn.back-btn");
+  backButton.addEventListener("click", function () {
+    window.location.href = "/home";
+  });
+
+  const settingsButton = document.querySelector(".btn.settings-btn");
+  settingsButton.addEventListener("click", function () {
+    window.location.href = "/profile_list/all";
+  });
+});
